93b7305f3fb249b490f476899d94c6509d71485f
Felipe Hanzava <fhanzava@hotmail.com>
2021-09-04 17:32:24 -0300

    created layout recycler view

--- app/src/main/java/one/digitalinnovation/todolist/MainActivity.kt
+++ app/src/main/java/one/digitalinnovation/todolist/MainActivity.kt
@@ -1,11 +1,27 @@
 package one.digitalinnovation.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import one.digitalinnovation.todolist.databinding.ActivityMainBinding
+import one.digitalinnovation.todolist.ui.AddTaskActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        insertListeners()
     }
+
+    private fun insertListeners() {
+        binding.buttonAdd.setOnClickListener {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
+    }
+
 }
