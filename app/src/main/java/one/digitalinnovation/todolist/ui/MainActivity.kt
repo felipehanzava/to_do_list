96ac928331f5326e03f8c75356f980807a441a50
Felipe Hanzava <fhanzava@hotmail.com>
2021-09-06 17:44:08 -0300

    created empty state

--- app/src/main/java/one/digitalinnovation/todolist/ui/MainActivity.kt
+++ app/src/main/java/one/digitalinnovation/todolist/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import one.digitalinnovation.todolist.databinding.ActivityMainBinding
 import one.digitalinnovation.todolist.datasource.TaskDataSource
 
@@ -48,7 +49,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateList(){
-        adapter.submitList(TaskDataSource.getList())
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility  =  if(list.isEmpty()){
+            View.VISIBLE
+        }else{
+            View.GONE
+        }
+        adapter.submitList(list)
     }
 
     companion object {
